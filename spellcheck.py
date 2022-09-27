c9e03672170e5684b538945ba28d16c4d44aa297
JaredB <bentvelj@mcmaster.ca>
2022-09-27 13:46:03 -0400

    adjustments

---
+++ spellcheck.py
@@ -0,0 +1,27 @@
+import subprocess
+import sys
+import known
+
+"""
+Run the book through a spell checker.
+
+Known exceptions are in `known.py`
+"""
+exit_code = 0
+
+files_to_check = sys.argv[1:] # Cut off script name
+
+for tex_path in files_to_check:
+
+    tex = tex_path.read_text()
+    aspell_output = subprocess.check_output(
+        ["aspell", "-t", "--list", "--lang=en_GB"], input=tex, text=True
+    )
+    incorrect_words = set(aspell_output.split("\n")) - {""} - known.words
+    if len(incorrect_words) > 0:
+        print(f"In {tex_path} the following words are not known: ")
+        for string in sorted(incorrect_words):
+            print(string)
+        exit_code = 1
+
+sys.exit(exit_code)
