13865b5caf954065fa1c5e508243f45a605d7b49
JaredB <bentvelj@mcmaster.ca>
2022-11-13 01:43:52 -0500

    Used lintstaged to keep linting and prettier on staged files only

--- src/athena/src/components/containers/Exercise.tsx
+++ src/athena/src/components/containers/Exercise.tsx
@@ -7,41 +7,53 @@ import { Card } from './Card';
 
 interface ExerciseProps {
   exerciseName: string;
-  muscles: (Muscle & { slug: UIMuscleSlugs })[];
+  muscles: (Muscle & {
+    slug: UIMuscleSlugs;
+  })[];
 }
 
 export const Exercise: React.FC<ExerciseProps> = ({ muscles, exerciseName }) => {
   return (
-    <Card style={{ display: 'flex', flexDirection: 'row' }}>
+    <Card
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+      }}>
       <View
         style={{
           width: 45,
           height: 45,
           borderRadius: 999,
           overflow: 'hidden',
-          position: 'relative'
-        }}
-      >
+          position: 'relative',
+        }}>
         <View
           style={{
             position: 'absolute',
             right: -8,
-            top: 0
-          }}
-        >
-          <Body
-            scale={0.6}
-            data={muscles}
-            frontOnly
-            colors={[theme.blue[600], theme.blue[300]]}
-          />
+            top: 0,
+          }}>
+          <Body scale={0.6} data={muscles} frontOnly colors={[theme.blue[600], theme.blue[300]]} />
         </View>
       </View>
 
       {/* main info column */}
-      <View style={{ marginLeft: 15, justifyContent: 'space-between', flex: 1 }}>
-        <Text style={{ fontWeight: '700' }}>{exerciseName}</Text>
-        <View style={{ flexDirection: 'row' }}>
+      <View
+        style={{
+          marginLeft: 15,
+          justifyContent: 'space-between',
+          flex: 1,
+        }}>
+        <Text
+          style={{
+            fontWeight: '700',
+          }}>
+          {exerciseName}
+        </Text>
+        <View
+          style={{
+            flexDirection: 'row',
+          }}>
           {muscles.map((muscle) => (
             <View
               key={muscle.slug}
@@ -52,17 +64,26 @@ export const Exercise: React.FC<ExerciseProps> = ({ muscles, exerciseName }) => 
                 paddingLeft: 5,
                 paddingRight: 5,
                 borderRadius: 4,
-                marginRight: 4
-              }}
-            >
-              <Text style={{ color: 'gray', fontSize: 10 }}>{muscle.slug}</Text>
+                marginRight: 4,
+              }}>
+              <Text
+                style={{
+                  color: 'gray',
+                  fontSize: 10,
+                }}>
+                {muscle.slug}
+              </Text>
             </View>
           ))}
         </View>
       </View>
 
       {/* chrevron column */}
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
         <ChevronRightIcon width={18} fill={theme.gray[200]} />
       </View>
     </Card>
