8c7daa04326b38fbf75f6aad2fb5901be8ede156
BasselR <basselrezkalla@gmail.com>
2023-02-12 14:27:16 -0500

    Add Discovery screen (Browse) to TabNavigator

--- src/athena/src/components/navigation/TabNavigator.tsx
+++ src/athena/src/components/navigation/TabNavigator.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home } from '../screens';
 import { Settings } from '../screens/Settings';
+import { Browse } from '../screens/Browse';
 
 export type TabParamList = {
   Home: undefined;
+  Browse: undefined;
   Settings: undefined;
 };
 const Tabs = createBottomTabNavigator<TabParamList>();
@@ -17,6 +19,7 @@ export const TabNavigator: React.FC = () => {
         headerShown: false,
       }}>
       <Tabs.Screen name="Home" component={Home} />
+      <Tabs.Screen name="Browse" component={Browse} />
       <Tabs.Screen name="Settings" component={Settings} />
     </Tabs.Navigator>
   );
