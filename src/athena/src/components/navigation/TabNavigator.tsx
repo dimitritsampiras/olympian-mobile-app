9920bcac7c350bab9000fbfd32d2d0a9f5a466d6
Will Lee <47044936+Wililee@users.noreply.github.com>
2023-02-11 20:04:45 -0500

    created icons and labels for custom tab bar (#156)

--- src/athena/src/components/navigation/TabNavigator.tsx
+++ src/athena/src/components/navigation/TabNavigator.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  HomeIcon,
+  GlobeAsiaAustraliaIcon,
+  BookOpenIcon,
+  UserIcon,
+} from 'react-native-heroicons/solid';
 import { Home } from '../screens';
 import { Settings } from '../screens/Settings';
 
 export type TabParamList = {
   Home: undefined;
   Settings: undefined;
+  Explore: undefined;
+  MyPrograms: undefined;
 };
 const Tabs = createBottomTabNavigator<TabParamList>();
 
@@ -16,8 +24,14 @@ export const TabNavigator: React.FC = () => {
       screenOptions={{
         headerShown: false,
       }}>
-      <Tabs.Screen name="Home" component={Home} />
-      <Tabs.Screen name="Settings" component={Settings} />
+      <Tabs.Screen name="Home" component={Home} options={{ tabBarIcon: HomeIcon }} />
+      <Tabs.Screen
+        name="Explore"
+        component={Home}
+        options={{ tabBarIcon: GlobeAsiaAustraliaIcon }}
+      />
+      <Tabs.Screen name="My Programs" component={Home} options={{ tabBarIcon: BookOpenIcon }} />
+      <Tabs.Screen name="Profile" component={Settings} options={{ tabBarIcon: UserIcon }} />
     </Tabs.Navigator>
   );
 };
