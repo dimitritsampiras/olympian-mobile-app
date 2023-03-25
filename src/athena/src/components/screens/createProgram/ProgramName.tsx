132f23ff8758243dd7c839cb78863453a2eb7c6b
Wililee <leewks@gmail.com>
2023-03-24 23:02:06 -0400

    keyboard now dismissed on creating a program

--- src/athena/src/components/screens/createProgram/ProgramName.tsx
+++ src/athena/src/components/screens/createProgram/ProgramName.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Keyboard, SafeAreaView, TouchableWithoutFeedback, View } from 'react-native';
 import { CreateProgramContext } from '../../../lib/context';
 
 import { Heading } from '../../elements';
@@ -16,18 +17,20 @@ export const ProgramName: React.FC<ProgramNameProps> = () => {
   };
 
   return (
-    <>
-      <Heading as="h2">Create your Program</Heading>
-      <BodyText style={{ width: 250, marginBottom: 24 }}>
-        Name your program and use our program creator to build a program tailored to your needs.
-      </BodyText>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={{ paddingBottom: 300 }}>
+        <Heading as="h2">Create your Program</Heading>
+        <BodyText style={{ width: 250, marginBottom: 24 }}>
+          Name your program and use our program creator to build a program tailored to your needs.
+        </BodyText>
 
-      <Input
-        value={program.name}
-        onChangeText={handleOnTextChange}
-        placeholder="push pull legs, upper lower, etc."
-        style={{ marginBottom: 30 }}
-      />
-    </>
+        <Input
+          value={program.name}
+          onChangeText={handleOnTextChange}
+          placeholder="push pull legs, upper lower, etc."
+          style={{ marginBottom: 30 }}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
