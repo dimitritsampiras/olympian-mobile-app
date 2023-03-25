9f1bfa98975d43f12bdf4e9d85e4c7f6dd04f254
Jared Bentvelsen <bentvelj@mcmaster.ca>
2023-03-25 11:16:31 -0400

    Goals profile (#253)
    * Goals work in progress
     #	modified:   src/athena/src/components/screens/auth/Login.tsx
    
    * Goals work in progress
     #	modified:   src/athena/src/components/screens/auth/Login.tsx
    
    * set goal page
    
    ---------
    
    Co-authored-by: Matthew_McCracken <mccrackenmatthew73@yahoo.ca>

--- src/athena/src/components/navigation/ProfileNavigator.tsx
+++ src/athena/src/components/navigation/ProfileNavigator.tsx
@@ -4,13 +4,20 @@ import { FollowingList } from '../screens/profile/FollowingList';
 import { FollowersList } from '../screens/profile/FollowersList';
 
 import { Socials } from '../screens/profile/Socials';
+import { Goals } from '../screens/profile/Goals';
+import { NewGoalExerciseSearch } from '../screens/profile/NewGoal';
 import { TabParamList } from './TabNavigator';
 import { Profile } from '../screens/profile/Profile';
 import { ProgramNavigator } from './ProgramNavigator';
+import { SetGoal } from '../screens/profile/SetGoal';
 
 export type ProfileParamList = {
   MyProfile: undefined;
   Goals: undefined;
+  // Choose the exercise
+  NewGoal: undefined;
+  // Set the reps & units
+  SetGoal: undefined;
   Socials: undefined;
   Activity: undefined;
   Account: undefined;
@@ -29,6 +36,9 @@ export const ProfileNavigator: React.FC<ProfileNavigatorProps> = () => {
     <ProfileStack.Navigator initialRouteName="MyProfile" screenOptions={{ headerShown: false }}>
       <ProfileStack.Screen name="MyProfile" component={MyProfile} />
       <ProfileStack.Screen name="Socials" component={Socials} />
+      <ProfileStack.Screen name="Goals" component={Goals} />
+      <ProfileStack.Screen name="NewGoal" component={NewGoalExerciseSearch} />
+      <ProfileStack.Screen name="SetGoal" component={SetGoal} />
       <ProfileStack.Screen name="FollowingList" component={FollowingList} />
       <ProfileStack.Screen name="FollowersList" component={FollowersList} />
       <ProfileStack.Screen name="Profile" component={Profile} />
