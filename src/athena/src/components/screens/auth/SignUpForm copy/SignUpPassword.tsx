01ef51fe967d8663a16d2ced7926850a367ab3b1
Dimitri Tsampiras <dimitri.tsampiras@gmail.com>
2023-04-11 00:36:31 -0400

    stuff

---
+++ src/athena/src/components/screens/auth/SignUpForm copy/SignUpPassword.tsx
@@ -0,0 +1,178 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { AuthParamList } from '../../../navigation';
+import { StyleSheet, Text, View } from 'react-native';
+import { useContext } from 'react';
+import { Input } from '../../../elements/Input';
+import theme from '../../../../theme';
+import { Formik } from 'formik';
+import { Button } from '../../../elements/Button';
+import TorchLogo from '../../../../../assets/TorchLogo.svg';
+import { PageControl } from 'react-native-ui-lib';
+import { object, ref, string } from 'yup';
+import { SignUpInput } from '../../../../lib/graphql';
+import { ClipboardDocumentCheckIcon, LockClosedIcon } from 'react-native-heroicons/solid';
+import { useSignUpMutation } from '../../../../lib/graphql';
+import { Heading } from '../../../elements/typography/Heading';
+import { BodyText } from '../../../elements/typography/BodyText';
+import { SignUpContext } from '../../../../lib/context';
+
+// This page will be doing navigating
+type ParamList = NativeStackScreenProps<AuthParamList, 'SignUp'>;
+interface SignUpPasswordProps extends ParamList {}
+
+interface SignUpInputConfirmPass extends SignUpInput {
+  confirm_password: string;
+}
+
+export const SignUpPassword: React.FC<SignUpPasswordProps> = ({ navigation }) => {
+  const [signup] = useSignUpMutation();
+  const { signUpInput, step } = useContext(SignUpContext);
+  const maxLength = 16;
+  const minLength = 4;
+  const passwordSchema = object({
+    password: string()
+      .required('Password cannot be empty.')
+      .min(minLength, `Password must be at least ${minLength} characters.`)
+      .max(maxLength, `Password must be at most ${maxLength} characters.`),
+
+    confirm_password: string()
+      .oneOf([ref('password'), null], 'Passwords must match.')
+      .required('Please confirm your password.'),
+  });
+
+  const handleOnNext = async (values: Partial<SignUpInputConfirmPass>) => {
+    // Error message better way to go?
+    // Wondering what the best way to enforce that all previous fields will be non-null
+    // They should never be null thanks to previous validations
+    // if (!signUpInput.name || !signUpInput.email || !signUpInput.username || !signUpInput.password){
+    //     console.log(`Attempted to create user failed, one or more fields is undefined.`)
+    // }
+    const signUpCreds = {
+      name: signUpInput.name,
+      email: signUpInput.email,
+      username: signUpInput.username,
+      // No need to update state with password before submission
+      password: values.password,
+    };
+    console.log(`Creating user: ${signUpCreds}`);
+
+    await signup({ variables: { input: signUpCreds as SignUpInput } });
+    navigation.navigate('Login');
+  };
+
+  return (
+    <Formik
+      initialValues={{ password: '', confirm_password: '' }}
+      onSubmit={handleOnNext}
+      validationSchema={passwordSchema}>
+      {({ handleSubmit, handleChange, values, errors, touched }) => {
+        return (
+          <>
+            <View style={styles.container}>
+              <View style={styles.innerContainer}>
+                {/* Sample SVG to be replaced with the actual torch once we have it*/}
+                <TorchLogo width={56} height={82} fill={'black'}></TorchLogo>
+                <Heading noMargin style={{ textAlign: 'center' }}>
+                  Password...Shhh
+                </Heading>
+                <BodyText style={{ textAlign: 'center' }}>
+                  {"Enter a strong password. Don't worry, you can recover it if you forget."}
+                </BodyText>
+                <Input
+                  placeholder="password"
+                  value={values.password}
+                  textContentType="password"
+                  secureTextEntry={true}
+                  onChangeText={handleChange('password')}
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                  error={touched.password && !!errors.password}
+                  style={styles.passwordField}
+                  Icon={LockClosedIcon}
+                  iconProps={{
+                    size: 20,
+                    fill: touched.password && !!errors.password ? 'red' : theme.colors.gray[400],
+                  }}
+                />
+                <Input
+                  placeholder="confirm password"
+                  value={values.confirm_password}
+                  textContentType="password"
+                  secureTextEntry={true}
+                  onChangeText={handleChange('confirm_password')}
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                  error={touched.confirm_password && !!errors.confirm_password}
+                  style={styles.passwordField}
+                  Icon={ClipboardDocumentCheckIcon}
+                  iconProps={{
+                    size: 20,
+                    fill:
+                      touched.confirm_password && !!errors.confirm_password
+                        ? 'red'
+                        : theme.colors.gray[400],
+                  }}
+                />
+                {/* Password error message takes priority */}
+                <Text style={styles.errorMessageStyle}>
+                  {touched.confirm_password && (errors.password || errors.confirm_password)}
+                </Text>
+              </View>
+              <View style={styles.footer}>
+                <PageControl
+                  color={theme.colors.blue[500]}
+                  inactiveColor={theme.colors.gray[200]}
+                  currentPage={step}
+                  numOfPages={4}
+                  limitShownPages
+                  spacing={8}
+                  size={8}
+                />
+                <Button style={{ padding: 19, width: '100%' }} onPress={handleSubmit as () => void}>
+                  Create Account
+                </Button>
+              </View>
+            </View>
+          </>
+        );
+      }}
+    </Formik>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  innerContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '60%',
+    width: '100%',
+  },
+  passwordField: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  footer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  errorMessageStyle: {
+    fontSize: 14,
+    color: 'red',
+    // Fix the lineHeight to prevent bumping when the text comes in
+    lineHeight: 13,
+  },
+});
--- src/athena/src/components/screens/auth/SignUpForm copy/SignUpUsername.tsx
+++ src/athena/src/components/screens/auth/SignUpForm copy/SignUpUsername.tsx
@@ -8,35 +8,49 @@ import TorchLogo from '../../../../../assets/TorchLogo.svg';
 import { PageControl } from 'react-native-ui-lib';
 import { object, string } from 'yup';
 import { SignUpInput } from '../../../../lib/graphql';
-import { AtSymbolIcon } from 'react-native-heroicons/solid';
-import { useEmailExistsLazyQuery } from '../../../../lib/graphql';
+import { UserCircleIcon } from 'react-native-heroicons/solid';
+import { useUserExistsLazyQuery } from '../../../../lib/graphql';
 import { Heading } from '../../../elements/typography/Heading';
 import { BodyText } from '../../../elements/typography/BodyText';
 import { SignUpContext } from '../../../../lib/context';
-import { SignUpFormProps } from './SignUp';
 
-export const SignUpEmail: React.FC<SignUpFormProps> = () => {
-  const { signUpInput, setSignUpInput, step, setStep } = useContext(SignUpContext);
-  const [findemail] = useEmailExistsLazyQuery();
-  const maxLength = 32;
-  const emailSchema = object({
-    email: string()
-      .required('Cannot have an empty email address.')
-      .email('Please enter a valid email address.')
+interface SignUpUsernameProps {
+  handleChange: {
+    (e: React.ChangeEvent<unknown>): void;
+    <T = string | React.ChangeEvent<unknown>>(field: T): T extends React.ChangeEvent<any>
+      ? void
+      : (e: string | React.ChangeEvent<unknown>) => void;
+  };
+}
+
+export const SignUpUsername: React.FC<SignUpUsernameProps> = () => {
+  const [findusername] = useUserExistsLazyQuery();
+  const { setSignUpInput, step, setStep } = useContext(SignUpContext);
+  const maxLength = 16;
+  const usernameSchema = object({
+    username: string()
+      .required('Cannot have an empty username.')
+      .matches(
+        /^[a-zA-Z0-9_]+$/,
+        'Username can only be composed of letters, numbers, and underscores.'
+      )
       .max(maxLength, `Please enter a maximum of ${maxLength} characters.`)
-      .test('', 'Email is already used on another account.', async (email) => {
-        const { data } = await findemail({ variables: { email: email || '' } });
-        return !data?.emailExists;
+      .test('', 'Username is already in use.', async (username) => {
+        const { data } = await findusername({ variables: { username: username || '' } });
+        return !data?.usernameExists;
       }),
   });
 
   const handleOnNext = (values: Partial<SignUpInput>) => {
-    setSignUpInput((previousValues) => ({ ...previousValues, email: values.email }));
+    setSignUpInput((previousValues) => ({ ...previousValues, username: values.username }));
     setStep(step + Number(step + 1 < 4));
   };
 
   return (
-    <Formik initialValues={{ email: '' }} onSubmit={handleOnNext} validationSchema={emailSchema}>
+    <Formik
+      initialValues={{ username: '' }}
+      onSubmit={handleOnNext}
+      validationSchema={usernameSchema}>
       {({ handleSubmit, handleChange, values, errors, touched }) => {
         return (
           <>
@@ -45,30 +59,27 @@ export const SignUpEmail: React.FC<SignUpFormProps> = () => {
                 {/* Sample SVG to be replaced with the actual torch once we have it*/}
                 <TorchLogo width={56} height={82} fill={'black'}></TorchLogo>
                 <Heading noMargin style={{ textAlign: 'center' }}>
-                  {' '}
-                  Hello, {signUpInput.name}{' '}
+                  Username Time
                 </Heading>
                 <BodyText style={{ textAlign: 'center' }}>
-                  {' '}
-                  Please enter your email address, just in case you forget your password.{' '}
+                  This will be the name you display publicly. Make it uniquely yours!
                 </BodyText>
+
                 <Input
-                  placeholder="email"
-                  value={values.email}
-                  onChangeText={handleChange('email')}
+                  placeholder="username"
+                  value={values.username}
+                  onChangeText={handleChange('username')}
                   autoCorrect={false}
                   autoCapitalize="none"
-                  error={touched.email && !!errors.email}
-                  style={styles.emailField}
-                  Icon={AtSymbolIcon}
+                  error={touched.username && !!errors.username}
+                  style={styles.usernameField}
+                  Icon={UserCircleIcon}
                   iconProps={{
                     size: 20,
-                    fill: touched.email && !!errors.email ? 'red' : theme.colors.gray[400],
+                    fill: touched.username && !!errors.username ? 'red' : theme.colors.gray[400],
                   }}
                 />
-                <Text style={styles.errorMessageStyle}>
-                  {(touched.email && errors.email) || ''}
-                </Text>
+                <Text style={styles.errorMessageStyle}>{touched.username && errors.username}</Text>
               </View>
               <View style={styles.footer}>
                 <PageControl
@@ -109,7 +120,7 @@ const styles = StyleSheet.create({
     height: '50%',
     width: '100%',
   },
-  emailField: {
+  usernameField: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
